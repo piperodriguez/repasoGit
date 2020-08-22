64af9d02391c8147bcd12a095c0a58fa683ae0c9
frodriguez <vargasjuan367@gmail.com>
2020-08-21 22:12:28 -0500

    se agrega estructura basica html

---
+++ form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>formulario</title>
+</head>
+<body>
+	<h1><?php echo "formulario"; ?></h1>
+	<form>
+		
+		<input type="text" name="nombre">
+		<br>
+		<button>Guardar</button>
+	</form>
+</body>
+</html>
